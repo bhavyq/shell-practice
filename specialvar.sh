908dff2aee79b78794ff7953d12a20f5e7bd7faf
Bhavya Sree <bhavyasreekosuru15@gmail.com>
2026-07-14 22:18:51 +0530

    script completed

---
+++ specialvar.sh
@@ -0,0 +1,10 @@
+#!/bin/bash  
+echo "All args passed to script: $@"
+echo "Number of vars passed to script: $#"
+echo "Script name: $0"
+echo "Present directory: $PWD"
+echo "who is running: $USER"
+echo "Home directory of current user: $HOME"
+echo "PID of this script: $$"
+sleep 100 &
+echo "All args passed to script : $*"
