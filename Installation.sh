d62f63f5cf7d51a963222a90d233926b09ab8133
Bhavya Sree <bhavyasreekosuru15@gmail.com>
2026-09-01 22:28:07 +0530

    Fix installation script

--- Installation.sh
+++ Installation.sh
@@ -3,6 +3,14 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
    echo "please run this with root user acess"
+   exit 1
 fi
 echo "installing ngnix"
 dnf install nginx -y
+
+if [ $? -ne 0 ]; then   # to check the exit code of above command
+   echo "installation failed"
+   exit 1
+else
+    echo "installation success"
+fi
