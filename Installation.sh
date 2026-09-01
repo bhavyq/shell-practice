ab50729a47b1efb0e873d6b836d1f4baa7b7105a
Bhavya Sree <bhavyasreekosuru15@gmail.com>
2026-09-01 21:51:48 +0530

    Fix installation script

--- Installation.sh
+++ Installation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 USERID=$(id -u)
 
-if [ USERID ne 0 ]; then
+if [ $USERID -ne 0 ]; then
    echo "please run this with root user acess"
 fi
 echo "installing ngnix"
-dnf install ngnix -y
+dnf install nginx -y
